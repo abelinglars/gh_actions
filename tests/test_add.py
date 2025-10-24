a5cbf5b323f322ea51cc5716928b3fa95ac09035
lars <larsabeling@protonmail.com>
2025-10-24 17:06:47 +0200

    removed failing test

--- tests/test_add.py
+++ tests/test_add.py
@@ -9,12 +9,5 @@ class TestAdd(TestCase):
         result = add(a, b)
         self.assertEqual(result, 4, "Summing 2 + 2 does not add up to 4")
 
-    def testFail(self):
-        a = 2
-        b = 2
-        result = add(a, b)
-        self.assertEqual(result, 5, "Wrong")
-
-
 if __name__ == "__main__":
     main()
