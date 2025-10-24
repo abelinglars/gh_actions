a24caeb98a092503d3cc5e8e08fe986aacdc8800
lars <larsabeling@protonmail.com>
2025-10-24 17:03:54 +0200

    initial commit

---
+++ tests/test_add.py
@@ -0,0 +1,20 @@
+from src.main import add
+from unittest import TestCase, main
+
+
+class TestAdd(TestCase):
+    def testAdd(self):
+        a = 2
+        b = 2
+        result = add(a, b)
+        self.assertEqual(result, 4, "Summing 2 + 2 does not add up to 4")
+
+    def testFail(self):
+        a = 2
+        b = 2
+        result = add(a, b)
+        self.assertEqual(result, 5, "Wrong")
+
+
+if __name__ == "__main__":
+    main()
